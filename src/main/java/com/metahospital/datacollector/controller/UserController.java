4e4e05208ea1628796abdc6ddb72ffad814c5c99
leext <68280060+SSCTuatara-Lee@users.noreply.github.com>
2021-12-17 17:15:16 +0800

    1.合并了datacontroller里的鉴权和usercontroller里的登录 2.我觉得登录成功后得在用户上传信息时给check端口验证登录信息再进行操作。 3.逻辑打包在loginsevice里了。

--- src/main/java/com/metahospital/datacollector/controller/UserController.java
+++ src/main/java/com/metahospital/datacollector/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.metahospital.datacollector.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.metahospital.datacollector.common.WechatUtil;
-import com.metahospital.datacollector.dao.WechatAccountDao;
-import com.metahospital.datacollector.dao.entity.WechatAccount;
+import com.metahospital.datacollector.common.RestResponse;
+import com.metahospital.datacollector.controller.dto.AuthReqDto;
+import com.metahospital.datacollector.controller.dto.AuthRspDto;
+import com.metahospital.datacollector.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
 
-import java.util.Random;
 /**
  * Created on 2021/12/12.
  */
@@ -18,21 +21,18 @@ import java.util.Random;
 
 @Controller
 public class UserController {
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     @Autowired
-    private WechatAccountDao wechatAccountDao;
+    private LoginService loginService;
+
     @PostMapping("wx/login")
     @ResponseBody
-    public void user_login(@RequestParam(value = "code", required = false) String code) {
-        JSONObject SessionKeyOpenId = WechatUtil.getSessionKeyOrOpenId(code);
-        String openid = SessionKeyOpenId.getString("openid");
-        //String unionid = SessionKeyOpenId.getString("unionid");//上了平台才会有值，私人开发返回null
-        String unionid = "aaa";
-        String sessionKey = SessionKeyOpenId.getString("session_key");
-        long userid = new Random().nextLong();
-        WechatAccount wechatAccount = new WechatAccount(openid,unionid,sessionKey,userid);
-        wechatAccountDao.replace(wechatAccount);
-
+    public RestResponse<AuthRspDto> user_login(@RequestParam(value = "code") String jsCode, HttpServletRequest request) {
+        AuthReqDto authReqDto = new AuthReqDto();
+        authReqDto.setWechatJsCode(jsCode);
+        String sessionId = request.getSession(true).getId();
+        LOGGER.info("auth api sessionid: " + sessionId);
+        return new RestResponse<>(loginService.UserMergeData(jsCode,sessionId));
     }
 }
 
