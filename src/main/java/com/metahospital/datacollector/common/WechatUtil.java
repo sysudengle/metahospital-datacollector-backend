811f2bdd1bc8c735e723f7faf3aefeed67573f2f
maxyaozou <maxyaozou@tencent.com>
2021-12-26 17:25:22 +0800

    feat: 更新小程序appid和secret

--- src/main/java/com/metahospital/datacollector/common/WechatUtil.java
+++ src/main/java/com/metahospital/datacollector/common/WechatUtil.java
@@ -16,9 +16,9 @@ public class WechatUtil {
         String requestUrl = "https://api.weixin.qq.com/sns/jscode2session";
         Map<String, String> requestUrlParam = new HashMap<>();
         //小程序appId//怕有危险的话，也可用@Value，这边是直接填写了
-        requestUrlParam.put("appid", "wx5b8dab5e67ea0800");
+        requestUrlParam.put("appid", "wx0c29b965eafb3618");
         //小程序secret
-        requestUrlParam.put("secret", "9c2b1eec5c5228b3f3de785ea48f7fd6");
+        requestUrlParam.put("secret", "7ca79cd4d587924ff9ff2eecd0df285f");
         //小程序端返回的code
         requestUrlParam.put("js_code", code);
         //默认参数
