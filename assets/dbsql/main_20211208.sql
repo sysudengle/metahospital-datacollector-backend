2ae8e1fbc82f9d902a83c4c5bac060d7a7287cbd
leext <68280060+SSCTuatara-Lee@users.noreply.github.com>
2021-12-27 22:33:35 +0800

    1.sql新增表和内容

--- assets/dbsql/main_20211208.sql
+++ assets/dbsql/main_20211208.sql
@@ -20,3 +20,27 @@ CREATE TABLE `user` (
 COMMENT='用户信息表'
 COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB;
+
+CREATE TABLE `inner_account` (
+    `inner_account` VARCHAR(32) NOT NULL COMMENT '内部账号' COLLATE 'utf8mb4_unicode_ci',
+    `password` VARCHAR(32) NOT NULL COMMENT '账号密码' COLLATE 'utf8mb4_unicode_ci',
+    `user_id` BIGINT(20) NOT NULL COMMENT '用户唯一Id',
+    PRIMARY KEY (`inner_account`) USING BTREE
+)
+    COMMENT='医院用户账号信息'
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
+
+
+CREATE TABLE `user_doctor_info` (
+   `user_id` BIGINT(20) NOT NULL COMMENT '用户唯一Id',
+   `hospital_id` INT(11) NOT NULL COMMENT '医院id',
+   `staff_id` VARCHAR(32) NOT NULL COMMENT '医生工号' COLLATE 'utf8mb4_unicode_ci',
+   `permission_state` INT(11) NOT NULL COMMENT '申请状态',
+   `department_permission` VARCHAR(32) NOT NULL COMMENT '科室权限' COLLATE 'utf8mb4_unicode_ci',
+   PRIMARY KEY (`user_id`) USING BTREE
+)
+    COMMENT='医院工号信息'
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
+
